00b9debace25ed4304c5d91dceacba9fe02a67de
Suryansh Niranjan <developerbilla2005@gmail.com>
2025-08-21 08:18:47 +0530

    Add files via upload

---
+++ Sheet 1/divisibleBy5And11.py
@@ -0,0 +1,7 @@
+# Take an integer A as input. You have to tell whether A is divisible by both 5 and 11 or 
+# not. 
+A = int(input("Enter a number: "))
+if A % 5 == 0 and A % 11 == 0:
+    print(A, "is divisible by both 5 and 11.")
+else:
+    print(A, "is not divisible by both 5 and 11.")
---
+++ Sheet 1/lastDigit4.py
@@ -0,0 +1,6 @@
+#check whether last digit of a number is 4
+num = int(input("Enter a number: "))
+if num % 10 == 4:
+    print("The last digit is 4.")
+else:
+    print("The last digit is not 4.")
